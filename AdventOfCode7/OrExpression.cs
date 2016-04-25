be9e99d8a9da8ce0d822e3964636f97c93b11070
Victor Grigoriu <vgrigoriu@gmail.com>
2016-04-25 21:45:48 +0300

    Add OR expression

---
+++ AdventOfCode7/OrExpression.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="OrExpression.cs" company="Victor Grigoriu (vgrigoriu@gmail.com)">
+// Copyright (c) Victor Grigoriu (vgrigoriu@gmail.com). All rights reserved.
+// </copyright>
+
+namespace AdventOfCode7
+{
+    /// <summary>
+    /// The bitwise OR of two variables
+    /// </summary>
+    internal sealed class OrExpression : BinaryExpression
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrExpression"/> class.
+        /// </summary>
+        /// <param name="firstOperand">The first operand</param>
+        /// <param name="secondOperand">The second operand</param>
+        public OrExpression(Variable firstOperand, Variable secondOperand)
+            : base(firstOperand, secondOperand)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override ushort GetValue()
+        {
+            // Obviously, this is not the correct implementation.
+            return 123;
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"{FirstOperand} OR {SecondOperand}";
+        }
+    }
+}
