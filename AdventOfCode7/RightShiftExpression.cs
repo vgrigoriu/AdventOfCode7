3378d98a7c75e08f764e4b5e9ef99930606dfec2
Victor Grigoriu <vgrigoriu@gmail.com>
2016-04-27 21:16:39 +0300

    Add right shift expression

---
+++ AdventOfCode7/RightShiftExpression.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="RightShiftExpression.cs" company="Victor Grigoriu (vgrigoriu@gmail.com)">
+// Copyright (c) Victor Grigoriu (vgrigoriu@gmail.com). All rights reserved.
+// </copyright>
+
+using System;
+
+namespace AdventOfCode7
+{
+    /// <summary>
+    /// Right shift
+    /// </summary>
+    internal sealed class RightShiftExpression : BinaryExpression
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RightShiftExpression"/> class.
+        /// </summary>
+        /// <param name="firstOperand">The variable</param>
+        /// <param name="secondOperand">How many bits to shift</param>
+        public RightShiftExpression(Variable firstOperand, ConstantExpression secondOperand)
+            : base(firstOperand, secondOperand)
+        {
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="NotImplementedException">Not yet.</exception>
+        public override ushort GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"{FirstOperand} >> {SecondOperand}";
+        }
+    }
+}
