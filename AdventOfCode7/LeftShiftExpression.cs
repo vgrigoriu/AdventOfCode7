1f49d644371509dc7eb1a13bdde99f3ffde7e208
Victor Grigoriu <vgrigoriu@gmail.com>
2016-04-27 21:15:14 +0300

    Add left shift expression.

---
+++ AdventOfCode7/LeftShiftExpression.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="LeftShiftExpression.cs" company="Victor Grigoriu (vgrigoriu@gmail.com)">
+// Copyright (c) Victor Grigoriu (vgrigoriu@gmail.com). All rights reserved.
+// </copyright>
+
+using System;
+
+namespace AdventOfCode7
+{
+    /// <summary>
+    /// Left shift
+    /// </summary>
+    internal sealed class LeftShiftExpression : BinaryExpression
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeftShiftExpression"/> class.
+        /// </summary>
+        /// <param name="firstOperand">The variable</param>
+        /// <param name="secondOperand">How many bits to shift</param>
+        public LeftShiftExpression(Variable firstOperand, ConstantExpression secondOperand)
+            : base(firstOperand, secondOperand)
+        {
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="NotImplementedException">Not yet.</exception>
+        public override ushort GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"{FirstOperand} << {SecondOperand}";
+        }
+    }
+}
